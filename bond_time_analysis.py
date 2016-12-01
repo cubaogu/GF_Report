eca551bc33a8564a923127cbf47ab814358f3070
chenchen <cubg5@sina.com>
2016-12-01 23:22:48 +0800

    1.try to identifying differences btw ABC 2.WATERFALL GRAPH FOR 150210

--- bond_time_analysis.py
+++ bond_time_analysis.py
@@ -15,6 +15,55 @@ import math
 mpl.rcParams['font.sans-serif'] = ['Microsoft YaHei']
 sns.set_context("talk")#这一部分是为了保证在画图时正常显示中文
 
+aDF = pd.read_excel("C:/Users/chenchen/Desktop/AtWork/TradeAnalysis/Bond2015/15A.xlsx")
+bDF = pd.read_excel("C:/Users/chenchen/Desktop/AtWork/TradeAnalysis/Bond2015/15B.xlsx")
+cDF = pd.read_excel("C:/Users/chenchen/Desktop/AtWork/TradeAnalysis/Bond2015/15C.xlsx")
+
+
+t1 = aDF['证券名称'].drop_duplicates()
+t2 = bDF['证券名称'].drop_duplicates()
+t3 = [x for x in t1 if x in t2]
+
+t4 = aDF[aDF['证券名称'].duplicated()]['证券名称'].drop_duplicates()
+
+t5 = aDF.groupby('证券名称')['券面总额(万元)'].count()
+t5.sort()
+
+aDF['方向'] = aDF['委托方向'].map(lambda x: 1 if x == '3-债券买入' else -1)
+aDF['仓位变化'] = aDF['方向']*aDF['券面总额(万元)'] 
+
+t7 = aDF.groupby('证券名称')['仓位变化'].sum() #券面总额(万元)
+t7.sort()
+
+t8 = aDF[aDF['证券名称'] == '15国开10']
+t8['收益率仓位加权'] = t8['收益率（%）']*t8['仓位变化']
+
+t9 = t8.groupby('成交日期')
+t10 = t9['仓位变化'].sum()
+sns.barplot(t10.index,t10.values)
+t11 = t9['收益率仓位加权'].sum()/t9['仓位变化'].sum()
+sns.plt(t11.values)
+
+blank = t10.cumsum().shift(1).fillna(0)
+step = blank.reset_index(drop=True).repeat(3).shift(-1)
+step[1::3] = np.nan
+
+
+#画持仓变化结合建仓点位的瀑布图
+fig = figure()
+ax1 = fig.add_subplot(111)
+
+my_plot = t10.plot(kind='bar', stacked=True, bottom=blank,legend=None, title="150210 各点位持仓变化瀑布图")
+my_plot.plot(step.index, step.values,'k')
+ax1.legend(loc = 'upper left')
+
+t11 = t9['收益率仓位加权'].sum()/t9['仓位变化'].sum()
+ax2=ax1.twinx() #双轴，右轴表示利率
+ax2.plot(t11.values,'rx-', label = '建仓平均点位(右轴)')
+ax2.legend(loc ='upper right')
+
+
+
 #先处理国债曲线
 intDF = pd.read_excel("C:/Users/chenchen/Desktop/AtWork/TradeAnalysis/YC/int.xlsx") #把excel中的数据再读出来
 t1 = intDF.T
